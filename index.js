82a2faa0e8ecddd69ba047af691d0bfc4304a30f
ntloff <72823447+ntloff@users.noreply.github.com>
2021-01-19 18:30:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-
+// by ntloff
+// my dc server: https://discord.gg/KHyy8d49FH
+//ez funcs
+function log(tolog){
+  console.log(tolog)
+}
+function randomnum(length) {
+return Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, length-1));
+}
+//mineflayer funcs
+function mfbot(ahost,aport,aun,av){
+  let bot = mineflayer.createBot({
+  host: ahost,
+  port: aport,
+  username: aun,
+  version: av
+})}
+function lookp(){
+  function (lookplayer){
+  const playerFilter = (entity) => entity.type === 'player'
+  const playerEntity = bot.nearestEntity(playerFilter)
+  if (!playerEntity) return
+  const pos = playerEntity.position.offset(0, playerEntity.height, 0)
+  bot.lookAt(pos) // after lookp() func create bot.on('physicTick', lookplayer)
+    }}
+}
+//discordjs funcs
+function msgtonchannel(channel,namee,msg){
+  let namee = client.channels.cache.find(namee => namee.namee === channel)
+  if(!namee)
+  namee.send(msg)
+}
